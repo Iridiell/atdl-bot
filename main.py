17d88bee23bd39f1c7d0724567bdd9196ccafb85
Iridiell <necrophilicbestialityy@gmail.com>
2022-03-23 00:29:48 +0530

    added scheduling

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ intents.guild_typing = False
 intents.dm_typing = False
 intents.presences = False
 intents.typing = False
-print("a")
 client = discord.Client(intents=intents)
 load_dotenv()
 # Getting ID from .env
@@ -34,11 +33,11 @@ async def on_ready():
     global atdl
     global iridiel
     global argodog
-    global role
+    global bot_role
     print('We have logged in as {0.user}'.format(client))
     atdl = client.get_guild(SERVER)
     bot_channel = atdl.get_channel(CHANNEL)
-    role = atdl.get_role(ROLE)
+    bot_role = atdl.get_role(ROLE)
     iridiel = client.get_user(IRIDIEL)
     argodog = client.get_user(422773482959667220)
 # on message function
@@ -48,7 +47,7 @@ async def on_message(message):
     global atdl
     global iridiel
     global argodog
-    global role
+    global bot_role
     if message.author == client.user:
         return
     
@@ -59,11 +58,30 @@ async def on_message(message):
             if "details" in message.content:
                 await message.channel.send(atdl)
                 await message.channel.send(bot_channel)
-                await message.channel.send(role)
+                await message.channel.send(bot_role)
         if message.content.startswith("$exit"):
             await client.close()
         if iridiel in message.mentions: 
             await message.channel.send("dont ping pls")
-        if role in message.role_mentions:
+        if message.content.startswith("$schedule"):
+            msg = message.content
+            split_msg = msg.strip().split(" ")
+            msg_mentions = message.role_mentions
+            msg_mentions.remove(bot_role)
+            msg_mentions = list(sorted(msg_mentions, key=lambda role: role.position))
+
+            if(len(msg_mentions)) != 3:
+                await message.channel.send("Need 3 parameters, 2 teams and division")
+            else:
+                team1 = msg_mentions[0]
+                team2 = msg_mentions[1]
+                division = msg_mentions[2]
+                time = split_msg[split_msg.index("GMT")-1]
+                await message.channel.send(team1.name)
+                await message.channel.send(team2.name)
+                await message.channel.send(division.name)
+                await message.channel.send(time)
+
+        if bot_role in message.role_mentions:
             await message.channel.send("you have pinged the bot role")
 client.run(TOKEN)
